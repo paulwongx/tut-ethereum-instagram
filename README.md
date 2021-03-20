e0f057f1442901a1024219a30caebb00fd1191f5
paulwongx <20178251+paulwongx@users.noreply.github.com>
2021-03-20 00:40:11 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tutorial
+
+[Tutorial Link](https://www.youtube.com/watch?v=XLahq4qyors)
+@2:39:45
--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,70 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  // Code goes here...
+    string public name = "Decentragram";
+
+    // Store Images
+    uint public imageCount = 0;
+    mapping(uint => Image) public images;
+
+    struct Image {
+        uint id;
+        string hash;
+        string description;
+        uint tipAmount;
+        address payable author;
+    }
+
+    event ImageCreated(
+        uint id,
+        string hash,
+        string description,
+        uint tipAmount,
+        address payable author
+    );
+
+    event ImageTipped(
+        uint id,
+        string hash,
+        string description,
+        uint tipAmount,
+        address payable author
+    );
+
+    // Create Images
+    function uploadImage(string memory _imgHash, string memory _description) public {
+        // Exceptions
+        require(bytes(_description).length > 0, 'Must include a description');
+        require(bytes(_imgHash).length > 0, 'Must include an image');
+        require(msg.sender != address(0x0), 'Must include a valid wallet address');
+
+        // Increment image id
+        imageCount ++;
+
+        // Add Image to contract
+        images[imageCount] = Image(imageCount, _imgHash, _description, 0, msg.sender);
+
+        // Trigger on event
+        emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
+    }
+
+    // Tip Images
+    // payable keyword allows you to send crypto in the function
+    function tipImageOwner(uint _id) public payable {
+        // Make sure the id is valid
+        require(_id > 0 && _id <= imageCount);
+        // Fetch the image
+        Image memory _image = images[_id];
+        // Fetch the author
+        address payable _author = _image.author;
+        // Pay the author by sending them Ether
+        address(_author).transfer(msg.value);
+        //Increment the tip amount
+        _image.tipAmount = _image.tipAmount + msg.value;
+        // Update the image
+        images[_id] = _image;
+
+        // Trigger an event
+        emit ImageTipped(_id, _image.hash, _image.description, _image.tipAmount, _author);
+    }
 }
